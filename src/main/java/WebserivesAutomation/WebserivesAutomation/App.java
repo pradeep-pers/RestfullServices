e49510e27e242ebfe736218991e8646d62b306af
pradeep-pers <prpradeep.panda@gmail.com>
2022-10-15 20:06:32 +0530

    APICODE

---
+++ src/main/java/WebserivesAutomation/WebserivesAutomation/App.java
@@ -0,0 +1,13 @@
+package WebserivesAutomation.WebserivesAutomation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
